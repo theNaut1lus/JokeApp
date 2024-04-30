9f9402f34d0abd558371b5ab063f9233e64fd4fe
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-04-30 18:03:54 +1000

    Initial Commit

---
+++ JokeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokeApp
+//
+//  Created by Sidak Singh Aulakh on 30/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokeApp/JokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAppApp.swift
+//  JokeApp
+//
+//  Created by Sidak Singh Aulakh on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct JokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
